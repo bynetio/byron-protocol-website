dad15530f4e83f3306a20981e3092b32d5ad9a67
Lucyna Borkowska <lucyna.borkowska@mail.com>
2022-05-05 14:10:35 +0200

    mobile ver

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,21 +9,31 @@ export default class Footer extends Component {
   render() {
     return (
       <div className="bg-bckg-blue">
-        <div className="max-w-6xl mx-auto py-6 flex">
-          <img src={logo} alt="Logo" className="sm:w-1/6"/>
-          <div className="sm:w-1/2">
-            <a href="/">Privacy Policy</a>
-            <a href="/">Terms & Conditions</a>
-            <a href="/">BinarApps</a>
-            <a href="/">IOHK</a>
+
+<div className="max-w-6xl mx-auto py-6">
+        <div className="grid grid-rows-2 grid-cols-6 grid-flow-col">
+          
+          <div className="row-span-2 col-span-1"><img src={logo} alt="Logo" className=""/></div>
+
+          <div className="row-span-1 col-span-3">
+            <a href="/" className="font-bold mr-4">Privacy Policy</a>
+            <a href="/" className="font-bold mr-4">Terms & Conditions</a>
+            <span className="mr-4 text-grey">|</span>
+            <a href="https://binarapps.com/" className="mr-4 text-blue" target="blank">BinarApps</a>
+            <a href="https://iog.io/" className="mr-4 text-blue" target="blank">IOG</a>
           </div>
-          <div className="sm:w-1/3 flex justify-end">
-            <a href="/"><img src={twitter} alt="Logo"/></a>
-            <a href="/"><img src={github} alt="Logo"/></a>
-            <a href="/"><img src={figma} alt="Logo"/></a>
-            <a href="/"><img src={instagram} alt="Logo"/></a>
+
+          <div className="row-span-1 col-span-3"><div className="mr-4 text-grey">Copyrignt@ 2022 Byron Network OU. All rights reserved.</div></div>
+
+          <div className="row-span-2 col-span-2 flex">
+            <a href="https://twitter.com/byron_cardano" target="blank"><img src={twitter} alt="Logo" className="mr-4"/></a>
+            <a href="https://github.com/byron-network/" target="blank"><img src={github} alt="Logo" className="mr-4"/></a>
+            <a href="https://discord.gg/Vp3w43Wt" target="blank"><img src={figma} alt="Logo" className="mr-4"/></a>
+            <a href="https://t.me/byron_network" target="blank"><img src={instagram} alt="Logo" className="mr-4"/></a>
           </div>
-          </div>
+        </div>
+
+        </div>
       </div>
     )
   }
--- src/components/Layers/index.js
+++ src/components/Layers/index.js
@@ -10,12 +10,12 @@ export default class Layers extends Component {
   render() {
     return (
       <section id="layers" className="bg-bckg-blue text-left">
-        <div className="flex max-w-6xl mx-auto">
-          <div className="w-full sm:w-1/2 mr-14">
+        <div className="flex mx-4 md:mx-0 md:max-w-6xl mx-auto">
+          <div className="w-full md:w-1/2 md:mr-14">
           <h4 className="text-sm text-blue uppercase font-bold tracking-[.5em] mb-6">Meet protocol layers</h4>
-          <h2 className="text-[56px] font-bold">On-chain Order-Book</h2>
+          <h2 className="text-[38px] md:text-[56px] font-bold">On-chain Order-Book</h2>
           <p className="mt-8 text-lg">Byron Network offers a fundamental on-chain order book called Bynet Protocol utilizing an infrastructure layer that enables development on top of the protocol. DApps can use shared liquidity from Bynet Protocol’s order book by developing their products with the infrastructure. Having mass adoption in mind, Byron Network aims to provide a development layer that is ready to be implemented by projects like BynetDEX.</p>
-          <div className="mt-12 text-lg">
+          <div className="mt-12 text-lg shadow">
             <div className="flex flex-row bg-white rounded-lg mb-6">
               <span className="basis-1/10 my-8 mx-6"><svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="16" cy="16" r="16" fill="#D7ECFF"/><path d="M21.3332 12L13.9998 19.3333L10.6665 16" stroke="#1566E1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></span>
 
@@ -37,57 +37,49 @@ export default class Layers extends Component {
                 </span>
                 <span className="basis-9/10 mt-6">
                 Adjustable Liquidity Orders distribution depending on the price dynamics
-                </span>
-            </div>
-
-
-            <h4 className="text-sm text-blue uppercase font-bold tracking-[.5em] mb-6 mt-52">Meet protocol layers</h4>
-            <h2 className="text-[56px] font-bold leading-relaxed">Matchmaking Engine</h2>
-            <p className="mt-8 text-lg">Matchmaking is a service that executes transactions by matching appropriate orders from the order book. Matchmaker is an algorithmic program that performs swaps while getting the difference between the bid and ask prices. It can decide on the commission they are aiming for, with diverse strategies dependent on a specific market.</p>
-            <p className="mt-8 text-blue">Stake Pool Operators are a crucial part of the Cardano network, and they will be incentivized to perform swaps as matchmakers with various options of rewarding their delegators.</p>
-
-            <h4 className="text-sm text-blue uppercase font-bold tracking-[.5em] mb-6 mt-52">Meet protocol layers</h4>
-            <h2 className="text-[56px] font-bold leading-relaxed">Problem solving with Bynet Protocol</h2>
-              <ul className="mt-8 text-lg">
-                <li>
-                <img src={dot1} alt="" ></img>
-                  <p className="text-xl font-bold mt-2 mb-4">Shared liquidity is the answer to new-born markets struggling with initial liquidity.</p>
-                  <p className="mb-8">Different projects can use liquidity on the Bynet Protocol’s On-chain Order Book and therefore increase the number of UTxOs a project has access to. They are not limited by their own market and take advantage of other contributors 
-                  to the order book.</p>
-                </li>
-                <li>
-                <img src={dot2} alt="" ></img>
-                  <p className="text-xl font-bold mt-2 mb-4">Easy way of deploying a DeFi project with a strong focus on the business model.</p>
-                  <p className="mb-8">Ever-changing trends in the blockchain space dictate a fast pace of project development. Our team has prepared advanced technology to lift the burden of technological research and to encourage teams to focus on innovative ways to utilize the trading potential. (kickstarter)</p>
-                </li>
-                <li>
-                <img src={dot3} alt="" ></img>
-                  <p className="text-xl font-bold mt-2 mb-4">Greater returns on investments thanks to Liquidity Order’s model</p>
-                  <p className="mb-8">With a model that gives the possibility to impose a fee on Liquidity Provider's orders and more projects utilizing its liquidity, higher returns can be expected. With more attractive orders, more swaps will be performed, 
-                  increasing the initial investment size.</p>
-                </li>
-              </ul>
-
+            </span></div>
           </div>
           </div>
-          <div className="w-full sm:w-1/2 ml-14">
-            <ul id="cards">
-            <li className="card" id="card_1">
-              <img className="card__content" src={a} alt="" ></img>
+          <div className="w-0 invisible md:visible md:w-1/2 md:ml-14"><img src={a} alt="" ></img></div>
+        </div>
+
+        <div className="flex max-w-6xl mx-auto mt-52">
+          <div className="w-full md:w-1/2 md:mr-14">
+          <h4 className="text-sm text-blue uppercase font-bold tracking-[.5em] mb-6">Meet protocol layers</h4>
+          <h2 className="text-[38px] md:text-[56px] font-bold">Matchmaking Engine</h2>
+          <p className="mt-8 text-lg">Matchmaking is a service that executes transactions by matching appropriate orders from the order book. Matchmaker is an algorithmic program that performs swaps while getting the difference between the bid and ask prices. It can decide on the commission they are aiming for, with diverse strategies dependent on a specific market.</p>
+          <p className="mt-8 text-blue">Stake Pool Operators are a crucial part of the Cardano network, and they will be incentivized to perform swaps as matchmakers with various options of rewarding their delegators.</p>
+          </div>
+          <div className="w-0 invisible md:visible md:w-1/2 md:ml-14"><img src={b} alt="" ></img></div>
+        </div>
+
+        <div className="flex max-w-6xl mx-auto mt-52">
+          <div className="w-full md:w-1/2 md:mr-14">
+          <h4 className="text-sm text-blue uppercase font-bold tracking-[.5em] mb-6">Meet protocol layers</h4>
+          <h2 className="text-[38px] md:text-[56px] font-bold">Problem solving with Bynet Protocol</h2>
+          <ul className="mt-8 text-lg">
+            <li>
+              <img src={dot1} alt=""/>
+              <p className="text-xl font-bold mt-2 mb-4">Shared liquidity is the answer to new-born markets struggling with initial liquidity.</p>
+              <p className="mb-8">Different projects can use liquidity on the Bynet Protocol’s On-chain Order Book and therefore increase the number of UTxOs a project has access to. They are not limited by their own market and take advantage of other contributors 
+              to the order book.</p>
             </li>
-            <li className="card" id="card_2">
-              <img className="card__content" src={b} alt="" ></img>
+            <li>
+            <img src={dot2} alt=""/>
+              <p className="text-xl font-bold mt-2 mb-4">Easy way of deploying a DeFi project with a strong focus on the business model.</p>
+              <p className="mb-8">Ever-changing trends in the blockchain space dictate a fast pace of project development. Our team has prepared advanced technology to lift the burden of technological research and to encourage teams to focus on innovative ways to utilize the trading potential. (kickstarter)</p>
             </li>
-            <li className="card" id="card_3">
-              <img className="card__content" src={c} alt="" ></img>
+            <li>
+            <img src={dot3} alt=""/>
+              <p className="text-xl font-bold mt-2 mb-4">Greater returns on investments thanks to Liquidity Order’s model</p>
+              <p className="mb-8">With a model that gives the possibility to impose a fee on Liquidity Provider's orders and more projects utilizing its liquidity, higher returns can be expected. With more attractive orders, more swaps will be performed, 
+              increasing the initial investment size.</p>
             </li>
           </ul>
           </div>
+          <div className="w-0 invisible md:visible md:w-1/2 md:ml-14"><img src={c} alt="" ></img></div>
         </div>
       </section>
     )
   }
 }
-
-
-          // <div className="w-full sm:w-1/2 ml-14"><img src={c} alt="" ></img></div>
