f6bbe42ca4bae24655b080e3679211c87fd84107
Lucyna Borkowska <lucyna.borkowska@mail.com>
2022-05-24 16:22:28 +0200

    layers wip

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import Nav from '../src/components/Nav';
 import ParallaxImagesContainer from '../src/components/Hero';
-import Layers from '../src/components/Layers';
+// import Layers from '../src/components/Layers';
 import CallToAction from '../src/components/CallToAction';
 import Footer from '../src/components/Footer';
+import LayersAnimation from './components/LayersAnimation';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
       <ParallaxImagesContainer/>
-      <Layers/>
+      <LayersAnimation/>
       <CallToAction/>
       <Footer/>
     </div>
